73bd20fb2706e7bcc2eccf3f6eb8277c1609a9f3
TrinhNgoc <jennyngu@hawaii.edu>
2015-01-10 13:26:08 -1000

    Added scripts to index.jade and centered title_screen

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -4,5 +4,16 @@ var paw = {};
 $(document).ready(function (){
   // initialize all main things,
 
-  
+  // Fadein and slidedown on title
+  $('#title_screen').stop(true).fadeIn({
+  	duration: 5000,
+  	queue: false
+  }).css('display', 'none').slideDown(1500);
+
+  //Start Button hides title_screen and show game_screen
+  $('.startbutton').click(function() {
+  	$('#title_screen').hide();
+  	$('#game_screen').css('visibility', 'visible');
+  });
+
 });
