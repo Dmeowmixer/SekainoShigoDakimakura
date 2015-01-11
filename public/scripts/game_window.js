2594656af58733693dbb3302130442426cd8e735
dmeowmixer <dmeowmixer@hotmail.com>
2015-01-10 20:31:12 -1000

    added all game.js locations jenny must style thems

--- public/scripts/game_window.js
+++ public/scripts/game_window.js
@@ -1,7 +1,11 @@
 $(function(){
+
   var gamewindow = $('#game_window');
-  $('subway_tunnel').html('Subway Tunnel');
-  $('unknown_tunnel').html('Unknown Tunnel');
-  $('Subway Entrance').html('Subway Entrance');
-  $('My House').html('My House');
-})
+
+  $('#subway_tunnel').html('Subway Tunnel');
+  $('#unknown_tunnel').html('Unknown Tunnel');
+  $('#subway_entrance').html('Subway Entrance');
+  $('#my_house').html('My House');
+  $('#temple').html('Temple');
+
+});
