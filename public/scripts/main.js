6283a65b8914ec2317ab727b241bea9be67bea41
TrinhNgoc <jennyngu@hawaii.edu>
2015-01-10 15:19:38 -1000

    Style title and buttons

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -4,11 +4,11 @@ var paw = {};
 $(document).ready(function (){
   // initialize all main things,
 
-  // Fadein and slidedown on title
-  $('#title_screen').stop(true).fadeIn({
-  	duration: 5000,
+  // Fadein on title
+  $('#title').stop(true).fadeIn({
+  	duration: 4000,
   	queue: false
-  }).css('display', 'none').slideDown(1500);
+  });
 
   //Start Button hides title_screen and show game_screen
   $('.startbutton').click(function() {
