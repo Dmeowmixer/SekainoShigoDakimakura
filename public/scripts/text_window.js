82def0ae8450456527b7824fa88d24139cf9215b
TrinhNgoc <jennyngu@hawaii.edu>
2015-01-10 20:28:19 -1000

    Text now shows

--- public/scripts/text_window.js
+++ public/scripts/text_window.js
@@ -1,11 +1,11 @@
 $(function() {
   var text_window = $('#text_window');
-  $(text_window).append('<p>', {
+  $(text_window).append($('<p>', {
     class: "npc_name",
-    html: "Ranson"
-  });
-  $(text_window).append('<p>', {
+    text: "Ranson"
+  }));
+  $(text_window).append($('<p>', {
     class: "dialogue",
-    html: "I will challege you to a duel!"
-  });
+    text: "I will challege you to a duel!"
+  }));
 });
