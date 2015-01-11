3917b331d66656fb86f1fb078cf22547eb8baaf6
TrinhNgoc <jennyngu@hawaii.edu>
2015-01-10 16:23:34 -1000

    Edited id names and fixed errors in js files

--- public/scripts/player_window.js
+++ public/scripts/player_window.js
@@ -1,29 +1,29 @@
 $(function(){
   var player_window = $('#player_window');
-  $('#player_name').text("Jon");
+  $('#_name').text("Jon");
 
-  $('#player_level').text("23");
+  $('#_level').text("23");
   
-  $('#player_health').text($('no hp is dedz')); 
+  $('#_health').text($('no hp is dedz')); 
   // var health = document.getElementById("health");
   // health.value = paw.Player.health.value; 
 
-  $('#player_exp').text($('go lvl u nubz'));
+  $('#_exp').text($('go lvl u nubz'));
   // var exp = document.getElementById("exp");
   // exp.value = paw.Player.exp.value;
 
 
-  $('#player_head').text("Turtle Helmet +3");
+  $('#_head').text("Turtle Helmet +3");
 
-  $('#player_torso').text("Platypus shirt +2");
+  $('#_torso').text("Platypus shirt +2");
 
-  $('#player_lefthand').text("Fork");
+  $('#_lefthand').text("Fork");
 
-  $('#player_righthand').text("Smelly Socks +4");
+  $('#_righthand').text("Smelly Socks +4");
 
-  $('#player_legs').text("Nothing");
+  $('#_legs').text("Nothing");
 
-  $('#player_feet').text("Kitten Mittens");
+  $('#_feet').text("Kitten Mittens");
 
 
 
