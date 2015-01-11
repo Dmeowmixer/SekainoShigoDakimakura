c695526ab64d49a7d562e7236a80b6ce7f465f8e
dmeowmixer <dmeowmixer@hotmail.com>
2015-01-10 16:32:33 -1000

    resolved conflicts

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -4,5 +4,16 @@ var paw = {};
 $(document).ready(function (){
   // initialize all main things,
 
-  
+  // Fadein on title
+  $('#title').stop(true).fadeIn({
+  	duration: 4000,
+  	queue: false
+  });
+
+  //Start Button hides title_screen and show game_screen
+  $('.startbutton').click(function() {
+  	$('#title_screen').hide();
+  	$('#game_screen').css('visibility', 'visible');
+  });
+
 });
