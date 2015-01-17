c6134eeb43bbf422551a6ec75995974e7bcc3bde
dmeowmixer <dmeowmixer@hotmail.com>
2015-01-17 00:04:34 -1000

    added battle screen js to index , moved player dead function, battle function, check health functions to battle_screen js from main.js working on check enemy hp and prompting if user would like to attack again until player/enemy death

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -6,51 +6,6 @@ $(document).ready(function (){
 
 
 
-/*
-
-
-  Player Dead function
-
-*/
-
-
-
-
-function deadplayer (player){
-  alert('You dead foo');
-  location.reload();
-}
-// checks players health within a function to see if player is still alive
-// runs deadplayer function if player reaches 0 health after enemy attack
-function checkHealth(player){
-  var health = player.health;
-  if (health <= 0){
-    deadplayer();
-  }
-}
-
-/*
-
-
-This will be moved? Battle function
-
-
-
-*/
- function battle(player, enemy){
-    if (player.health > 0 && enemy.health > 0){
-      var hp_left = player.health - enemy.damage;
-      player.health = hp_left;
-      checkHealth(window.player);
-      return "Playa Has Taken " + enemy.damage + "  damage";
-    }
-    if (player.health <= 0){
-      deadplayer();
-    }
- }
-
-
-
 
 
 
