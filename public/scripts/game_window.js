a57e034dc7dd423e53a9df70e6c94687a9fafd02
TrinhNgoc <jennyngu@hawaii.edu>
2015-01-17 19:50:59 -1000

    Added images and completed the map

--- public/scripts/game_window.js
+++ public/scripts/game_window.js
@@ -2,11 +2,11 @@ $(function(){
 
   var gamewindow = $('#game_window');
 
-  $('#subway_tunnel').html('Subway Tunnel');
-  $('#unknown_tunnel').html('Unknown Tunnel');
-  $('#subway_entrance').html('Subway Entrance');
-  $('#my_house').html('My House');
-  $('#temple').html('Temple');
+  $('#subway_tunnel').append('<p>Subway Tunnel</p>');
+  $('#unknown_tunnel').append('<p>Unknown Tunnel</p>');
+  $('#subway_entrance').append('<p>Subway Entrance</p>');
+  $('#my_house').append('<p>My House</p>');
+  $('#temple').append('<p>Temple</p>');
 
 
 
