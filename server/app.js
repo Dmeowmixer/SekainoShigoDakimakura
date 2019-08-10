d7f0f86ff66630517ea0fd795ed2e76a29a6d47f
dmeowmixer <ransonikuo@gmail.com>
2019-08-09 22:34:26 -1000

    refreshing project

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-// app.set('view engine', 'jade');
+app.set('view engine', 'jade');
 
 app.use(express.static(__dirname + '/../public'));
 
